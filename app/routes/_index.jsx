fe271d97f4f88fee3813ace09c92599b45668db9
linhken <silinh66@gmail.com>
2025-12-29 22:33:00 +0700

    Fix syntax error in _index.jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,10 +2,10 @@ import { redirect } from "@remix-run/node";
 import { login } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-    const url = new URL(request.url);
+  const url = new URL(request.url);
 
-    if (url.searchParams.get("shop")) {
-        throw redirect(\`/app?${url.searchParams.toString()}\`);
+  if (url.searchParams.get("shop")) {
+    throw redirect(`/app?${url.searchParams.toString()}`);
   }
 
   return await login(request);
