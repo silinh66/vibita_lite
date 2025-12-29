041595880157b1aa3c39a1c008b64d6703dac345
linhken <silinh66@gmail.com>
2025-12-29 22:23:58 +0700

    Add root redirect to fix white screen (cleaned)

---
+++ app/routes/_index.jsx
@@ -0,0 +1,16 @@
+import { redirect } from "@remix-run/node";
+import { login } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+    const url = new URL(request.url);
+
+    if (url.searchParams.get("shop")) {
+        throw redirect(\`/app?${url.searchParams.toString()}\`);
+  }
+
+  return await login(request);
+};
+
+export default function Index() {
+  return null;
+}
